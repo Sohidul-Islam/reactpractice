fe85848d5d64c510b6cdb985e8ce00255db2837c
Sohidul Islam <sishufol.sim@gmail.com>
2022-05-22 01:09:12 +0600

    data load by using api and use useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,43 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
 
   return (
     <div className="App">
-      <Counter><h1>hello</h1></Counter>
+      <ExternalUsers></ExternalUsers>
     </div >
   );
 }
 
+function ExternalUsers() {
+  //1st step
+  const [users, setUsers] = useState([])
+  // use useEffect function to data load.
+  useEffect(() => {
+    console.log("inside use effect");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUsers(data))
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
-  //counter as a variable
-  //setCounter as function that change the state variable.
-  const incrementCounter = () => {
-    //  method 1
-    // setCounter(counter + 1)
-    const newCounter = counter + 1;
-    // method 2
-    setCounter(newCounter)
-  }
-  const decrementCounter = () => {
-    setCounter(counter - 1)
-  }
+    console.log("Users: ", users);
+  }, [])
   return (
-    <div>
-      <h1>Counte: {counter}</h1>
-      {/* method 1 */}
-      {/* <button onClick={incrementCounter}>Increased</button> */}
-      {/* method 2 */}
-      <button onClick={() => {
-        setCounter(counter - 1)
-      }}>Increased</button>
-      <button onClick={decrementCounter}>Decreased</button>
+    <div><h2>External Users</h2>
+      {users.map(user => <Users name={user.name} email={user.email} city={user.address.city}></Users>)}
     </div>
   )
 }
+
+
+function Users(props) {
+  return (<div className="person-box">
+    <h3>Name: {props.name}</h3>
+    <h3>Email: {props.email}</h3>
+    <h3>City: {props.city}</h3>
+  </div>)
+}
+
 export default App;
 
+
+// function Counter() {
+//   const [counter, setCounter] = useState(0);
+//   //counter as a variable
+//   //setCounter as function that change the state variable.
+//   const incrementCounter = () => {
+//     //  method 1
+//     // setCounter(counter + 1)
+//     const newCounter = counter + 1;
+//     // method 2
+//     setCounter(newCounter)
+//   }
+//   const decrementCounter = () => {
+//     setCounter(counter - 1)
+//   }
+//   return (
+//     <div>
+//       <h1>Counte: {counter}</h1>
+//       {/* method 1 */}
+//       {/* <button onClick={incrementCounter}>Increased</button> */}
+//       {/* method 2 */}
+//       <button onClick={() => {
+//         setCounter(counter - 1)
+//       }}>Increased</button>
+//       <button onClick={decrementCounter}>Decreased</button>
+//     </div>
+//   )
+// }
