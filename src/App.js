23b3f3acaed38d7306ab808bc3a8e273a5c27914
Sohidul Islam <sishufol.sim@gmail.com>
2022-05-21 16:52:30 +0600

    dynamic data load

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ function App() {
 
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-    </div>
+      <Person name="Sohidul Islam" profession="Web developer" ></Person>
+      <Person name="Owahidul Hoque Chowdhury" profession="App developer"></Person>
+      <Person name="Kaium Uddin" profession="Web devloper"></Person>
+    </div >
   );
 }
 
-function Person() {
-  const personStyle = {
-    backgroundColor: "beige",
-    color: "black",
-    margin: "20px",
-    padding: "20px",
-    borderRadius: "20px",
-    border: "1px solid black",
-  }
+function Person(props) {
+  // const personStyle = {
+  //   backgroundColor: "beige",
+  //   color: "black",
+  //   margin: "20px",
+  //   padding: "20px",
+  //   borderRadius: "20px",
+  //   border: "1px solid black",
+  // }
   return (
     // <div className="person-box"> adding className as person box
     // <div style={personStyle}> adding style object
@@ -32,8 +33,8 @@ function Person() {
       borderRadius: "20px",
       border: "1px solid black",
     }}>
-      <h1>Name: Sohidul Islam</h1>
-      <h3>Profession: Developer</h3>
+      <h1>Name: {props.name}</h1>
+      <h3>Profession: {props.profession}</h3>
     </div>
   );
 }
