a93e330f56dce5d658ba20db1e4d6d18780e7b77
Sohidul Islam <sishufol.sim@gmail.com>
2022-06-24 15:39:51 +0600

    onBlur handler

--- src/components/Form/Loginform.js
+++ src/components/Form/Loginform.js
@@ -9,13 +9,13 @@ const Loginform = (props) => {
                 <div className="row mb-3">
                     <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-10">
-                        <input onChange={email} type="email" className="form-control" id="inputEmail3" />
+                        <input onBlur={email} type="email" className="form-control" id="inputEmail3" />
                     </div>
                 </div>
                 <div className="row mb-3">
                     <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-10">
-                        <input onChange={pass} type="password" className="form-control" id="inputPassword3" />
+                        <input onBlur={pass} type="password" className="form-control" id="inputPassword3" />
                     </div>
                 </div>
 
